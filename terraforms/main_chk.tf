43de090b0a65d127f865056449136bbbb6200d52
dev1 <dev1@gmail.com>
2024-12-02 21:15:12 +0530

    created terraforms directory and  main_chk.tf file

---
+++ terraforms/main_chk.tf
@@ -0,0 +1,90 @@
+#provider details
+provider "aws" {
+  region = "ap-southeast-1"
+  alias = "west"
+}
+
+# Fetch existing key pair or create a new one
+data "aws_key_pair" "existing_key" {
+  key_name = "terraformkey"
+}
+
+resource "aws_key_pair" "key_pair_2" {
+  count      = length(data.aws_key_pair.existing_key.*.id) == 0 ? 1 : 0
+  key_name   = "terraformkey"
+  provider = aws.west 
+  public_key = file("${path.cwd}/keys/id_rsa.pub")
+}
+
+# Fetch existing security group or create a new one
+data "aws_security_group" "existing_security_group" {
+  filter {
+    name   = "group-name"
+    values = ["example_security_group"]
+  }
+}
+
+resource "aws_security_group" "example_security_group_2" {
+  count       = length(data.aws_security_group.existing_security_group.*.id) == 0 ? 1 : 0
+  name        = "example_security_group"
+  description = "Allow SSH and HTTP traffic"
+  provider = aws.west 
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # opens SSH to public
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # opens HTTP to public
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Define EC2 instance
+resource "aws_instance" "terraform_demo_2" {
+  ami           = "ami-0ef1a6822965893ba"
+  instance_type = "t2.micro"
+
+  # Use existing or newly created security group
+  vpc_security_group_ids = [
+    coalesce(
+      data.aws_security_group.existing_security_group.id,
+      aws_security_group.example_security_group.*.id[0]
+    )
+  ]
+
+  # Use existing or newly created key pair
+  key_name = coalesce(
+    data.aws_key_pair.existing_key.key_name,
+    aws_key_pair.key_pair.*.key_name[0]
+  )
+
+  tags = {
+    Name        = "TERRAFORM_INSTANCE"
+    Environment = "Development"
+  }
+}
+
+# Output public IP address
+output "aws_public_ip_2" {
+  value       = aws_instance.terraform_demo_2.public_ip
+  description = "Public IP to connect to the EC2 instance"
+}
+
+# Output private IP address
+output "aws_private_ip_2" {
+  value       = aws_instance.terraform_demo_2.private_ip
+  description = "Private IP to connect to the EC2 instance"
+}
